1d6c337a54fa1ce14ad98ca016097c96c212ddfb
Виктор Бахирев <v_bahirev@MacBook-Air-Viktor.local>
2026-03-05 17:21:36 +1000

    Updates

---
+++ assets/chunk-c3DzR2oe.js
@@ -0,0 +1,1 @@
+import{g as u}from"./chunk-wt6bhfiO.js";import{c as e}from"./chunk-C8F1jNx6.js";function l(o="laser"){return e(()=>u(o)).value.use()}export{l as u};
--- assets/chunk-vi9cYVYF.js
+++
@@ -1,1 +0,0 @@
-import{g as u}from"./chunk-CihEqA_-.js";import{c as e}from"./chunk-QAgcm2Kj.js";function l(o="laser"){return e(()=>u(o)).value.use()}export{l as u};
